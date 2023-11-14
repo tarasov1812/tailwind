90f415712e622c8b9c9ab61299415af6e6354987
tarasov1812 <tarasov1812@gmail.com>
2023-11-14 13:08:34 +0100

    fix14

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // build: {
-  //   outDir: './src',
-  //   emptyOutDir: false,
-  // },
+  build: {
+    outDir: '/',
+    emptyOutDir: false,
+  },
   plugins: [react()]
 })
